a4ae0905b1d4e3a173d20fa287c2efd558e3867c
GeraldynS <semblantesnicol@gmail.com>
2024-08-14 10:51:42 -0400

    Creacion HomePage, UserForm new

---
+++ src/Pages/HomePage/index.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import Header from '../../components/Header';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBuilding, faFileContract, faPuzzlePiece } from "@fortawesome/free-solid-svg-icons";
+import "./HomePage.css";
+
+const HomePage = () => {
+    return (
+        <div className="home-container">
+            {/* Renderiza el Header */}
+            <Header />
+
+            <div className="home_modules-container">
+                <div className="module_item-container">
+                    <div className="small-module" style={{ backgroundColor:"#071952" }}>
+                        <FontAwesomeIcon icon={faBuilding} className="icono"  />
+                    </div>
+                    <div className="content">
+                        <div className="content-header">
+                            <p className="content-title">Compañias</p>
+                            <p className="content-count">200</p>
+                        </div>
+                        <hr />
+                        <div className="content-footer">
+                            <p className="content-stadistic"></p>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="module_item-container">
+                    <div className="small-module" style={{ backgroundColor:"#088395" }}>
+                        <FontAwesomeIcon icon={faFileContract} className="icono" />
+                    </div>
+                    <div className="content">
+                        <div className="content-header">
+                            <p className="content-title">Contratos</p>
+                            <p className="content-count">200</p>
+                        </div>
+                        <hr />
+                        <div className="content-footer">
+                            <p className="content-stadistic"></p>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="module_item-container">
+                    <div className="small-module" style={{ backgroundColor:"#37B7C3" }} >
+                        <FontAwesomeIcon icon={faPuzzlePiece} className="icono" />
+                    </div>
+                    <div className="content">
+                        <div className="content-header">
+                            <p className="content-title">Modulos</p>
+                            <p className="content-count">200</p>
+                        </div>
+                        <hr />
+                        <div className="content-footer">
+                            <p className="content-stadistic"></p>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div >
+    );
+};
+
+export default HomePage;
